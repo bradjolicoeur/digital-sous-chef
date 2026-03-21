88ff1123e12e843b41b43f231aef1d29e47580c2
Brad Jolicoeur <brad@jolicoeurs.net>
2026-03-21 18:50:25 -0400

    add postgres

--- src/DigitalSousChef/DigitalSousChef.AppHost/AppHost.cs
+++ src/DigitalSousChef/DigitalSousChef.AppHost/AppHost.cs
@@ -1,6 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.DigitalSousChef_Server>("digitalsouschef-server");
+var postgres = builder.AddPostgres("postgres")
+    .WithDataVolume()
+    .WithPgAdmin();
+
+var martenDb = postgres.AddDatabase("marten");
+
+var apiService = builder.AddProject<Projects.DigitalSousChef_Server>("digitalsouschef-server")
+    .WithReference(martenDb)
+    .WaitFor(martenDb);
 
 builder.AddNpmApp("digitalsouschef-client", "../digitalsouschef.client", "dev")
     .WithReference(apiService)
