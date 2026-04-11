25f724ce1a5e7990fbfdf671032f15c47690bc13
Brad Jolicoeur <brad@jolicoeurs.net>
2026-04-11 11:07:43 -0400

    fix reference and add wolverine otel

--- src/DigitalSousChef/DigitalSousChef.ServiceDefaults/Extensions.cs
+++ src/DigitalSousChef/DigitalSousChef.ServiceDefaults/Extensions.cs
@@ -62,6 +62,7 @@ public static class Extensions
             .WithTracing(tracing =>
             {
                 tracing.AddSource(builder.Environment.ApplicationName)
+                    .AddSource("WolverineFx")
                     .AddAspNetCoreInstrumentation(tracing =>
                         // Exclude health check requests from tracing
                         tracing.Filter = context =>
